46976a0e844ebe1cc1f15b99ba2b7317d0fd0b0f
haovo234 <vnhao23@clc.fitus.edu.vn>
2025-11-08 21:20:42 +0700

    ISOLATION LEVEL

--- DIRTY READ/ERROR/T2.sql
+++ DIRTY READ/ERROR/T2.sql
@@ -1,6 +1,7 @@
-﻿USE KHOHANG
+﻿-- Vì isolation level mặc định của ssms là read committed nên phải set isolation level là read uncommited hoặc đọc với nolock để mô phỏng
+USE KHOHANG
 GO
-
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
 BEGIN TRAN
-SELECT * FROM SANPHAM WITH (NOLOCK) WHERE MASP = 'SP01'
+SELECT * FROM SANPHAM WHERE MASP = 'SP01'
 COMMIT TRAN
